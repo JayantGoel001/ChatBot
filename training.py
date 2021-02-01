fb4168b57cd3c9d729b965dc552b635396d6d6c0
username <54479676+CormacKrum@users.noreply.github.com>
2021-02-02 01:47:08 +0530

    Initial Commit

--- training.py
+++ training.py
@@ -2,7 +2,19 @@ import json
 import random
 import pickle
 import numpy as np
+
 import nltk
+from nltk.stem import WordNetLemmatizer
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activation, Dropout
+from tensorflow.keras.optimizers import SGD
+
+lemmatizer = WordNetLemmatizer()
+
+intents = {}
+with open("intents.json", "r+") as f:
+    intents = json.loads(f.read())
 
 
 
