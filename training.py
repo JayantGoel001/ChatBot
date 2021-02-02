0439f79de01cfb1dd67b4bb71662e517d4029bf1
username <54479676+CormacKrum@users.noreply.github.com>
2021-02-03 00:21:50 +0530

    Initial Commit

--- training.py
+++ training.py
@@ -7,7 +7,7 @@ import nltk
 from nltk.stem import WordNetLemmatizer
 
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Activation, Dropout
+from tensorflow.keras.layers import Dense, Dropout
 from tensorflow.keras.optimizers import SGD
 
 lemmatizer = WordNetLemmatizer()
@@ -48,9 +48,27 @@ for document in documents:
     word_patterns = document[0]
     word_patterns = [lemmatizer.lemmatize(word.lower()) for word in word_patterns]
     for word in words:
-        bag.append(word) if word in word_patterns else bag.append(0)
+        bag.append(1) if word in word_patterns else bag.append(0)
 
     output_row = list(output)
     output_row[classes.index(document[1])] = 1
     training.append([bag, output_row])
 
+random.shuffle(training)
+training = np.array(training)
+
+X_train = list(training[:, 0])
+y_train = list(training[:, 1])
+
+model = Sequential()
+model.add(Dense(128, input_shape=(len(X_train[0]),), activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(y_train[0]), activation='softmax'))
+
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+model.fit(np.array(X_train), np.array(y_train), epochs=200, batch_size=5, verbose=1)
+model.save("chatbot.model")
